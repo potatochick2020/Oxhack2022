8bf44e128d3a3b929ea4a7631a70c386dadd50c3
potatochick2020 <potatochick2020@gmail.com>
2022-11-12 14:58:41 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import SimpleMap from "./MainMapBlock";
 import Container from "@mui/material/Container";
 import ButtonAppBar from "./components/Navbar/Navbar";
-
+import Grid from "@mui/material/Grid";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Container maxWidth="xl">
-    <ButtonAppBar/>
-    <SimpleMap />
-  </Container>
+  <>
+    <ButtonAppBar />
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <SimpleMap />
+      </Grid>
+      <Grid item xs={4}></Grid>
+    </Grid>
+  </>
+  // <Container maxWidth="xl">
+  //
+  //
+  // </Container>
 );
