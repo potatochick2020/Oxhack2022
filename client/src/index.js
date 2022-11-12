51bb7e006aa0ef95a8bba4b9f46b6402fef1d92c
potatochick2020 <potatochick2020@gmail.com>
2022-11-12 14:58:41 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import GoGoMap from "./MainMapBlock";
 import Container from "@mui/material/Container";
 import ButtonAppBar from "./components/Navbar/Navbar";
+ 
+import Grid from "@mui/material/Grid";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Container maxWidth="xl">
+  <>
     <ButtonAppBar />
-    <GoGoMap />
-  </Container>
+    <Grid container spacing={2}>
+      <Grid item xs={8} style={{height:"200px"}}>
+        <GoGoMap />
+      </Grid>
+      <Grid item xs={4} style={{backgroundColor:"Black"}}></Grid>
+    </Grid>
+  </>
+  // <Container maxWidth="xl">
+  //
+  //
+  // </Container>
 );
