ff77949244753266e798288c1d3d08542f7d421c
potatochick2020 <potatochick2020@gmail.com>
2022-11-12 14:52:11 +0000

    update on layout

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';   
+import Modal from '@mui/material/Modal';
+import AddressForm from '../Form/Form';
+const style = {
+    position: 'absolute' ,
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+export default function ButtonAppBar() {
+    const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+           
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            JFFT
+          </Typography>
+          <Button style={{color:"white"}} onClick={handleOpen}>Open modal</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+          <AddressForm />
+        </Box>
+      </Modal>
+    </>
+  );
+}
