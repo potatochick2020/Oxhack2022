a43817859966e527f5f9d2ab5c408f3a5d183ce5
SkollRyu <sixmilk0128@gmail.com>
2022-11-12 15:16:10 +0000

    fix: package, doing map

--- client/src/MainMapBlock.jsx
+++ client/src/MainMapBlock.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import "../../styles/global.css";
+
 export default function GoGoMap() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyCJdsRVRo0TL3n0B3X9gYliG2nSESA-ERc",
@@ -14,14 +14,8 @@ function Map() {
   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
 
   return (
-    <>
-      <GoogleMap
-        zoom={10}
-        center={center}
-        mapContainerClassName="map-container"
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </>
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
+    </GoogleMap>
   );
 }
