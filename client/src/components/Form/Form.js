0f412eaae18a5d0e0392cf39a5e4e19b376ff200
potatochick2020 <potatochick2020@gmail.com>
2022-11-12 14:52:11 +0000

    update on layout

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import './styles.css'
+
+export default function AddressForm(){
+
+    const [formValues, setFormValues] = useState([{ Address: ""}])
+
+    let handleChange = (i, e) => {
+        let newFormValues = [...formValues];
+        newFormValues[i][e.target.name] = e.target.value;
+        setFormValues(newFormValues);
+      }
+    
+    let addFormFields = () => {
+        setFormValues([...formValues, { Address: "" }])
+      }
+    
+    let removeFormFields = (i) => {
+        let newFormValues = [...formValues];
+        newFormValues.splice(i, 1);
+        setFormValues(newFormValues)
+    }
+    
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        alert(JSON.stringify(formValues));
+    }
+
+    return (
+        <form  onSubmit={handleSubmit}>
+          {formValues.map((element, index) => (
+            <div className="form-inline" key={index}> 
+              <label>Address</label>
+              <input type="text" name="Address" value={element.Address || ""} onChange={e => handleChange(index, e)} />
+              {
+                index ? 
+                  <button type="button"  className="button remove" onClick={() => removeFormFields(index)}>Remove</button> 
+                : null
+              }
+            </div>
+          ))}
+          <div className="button-section">
+              <button className="button add" type="button" onClick={() => addFormFields()}>Add</button>
+              <button className="button submit" type="submit">Submit</button>
+          </div>
+      </form>
+    )
+}
+
+ 
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import SimpleMap from "./MainMapBlock";
 import Container from "@mui/material/Container";
+import ButtonAppBar from "./components/Navbar/Navbar";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Container maxWidth="xl">
+    <ButtonAppBar/>
     <SimpleMap />
   </Container>
 );
