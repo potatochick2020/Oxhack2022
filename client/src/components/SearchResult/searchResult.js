7e63d0254da90f078c9bedf3ade75132654c56cb
SkollRyu <sixmilk0128@gmail.com>
2022-11-13 03:01:46 +0000

    feat: make each row as a box for each place

--- client/src/components/SearchResult/searchResult.js
+++ client/src/components/SearchResult/searchResult.js
@@ -1,15 +1,23 @@
 import React from "react";
 import getPlaces from "../../api";
 import { useState, useEffect } from "react";
+import { Stack } from "react-bootstrap";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function SearchResult({ Address, keyword, country }) {
   const [data, setData] = useState([]);
 
-  const data_places = getPlaces(
-    Address,
-    keyword,
-    country 
-  );
+  const data_places = getPlaces(Address, keyword, country);
 
   // change promise object to array
   useEffect(() => {
@@ -22,16 +30,22 @@ function SearchResult({ Address, keyword, country }) {
 
   return (
     // print out the data
-  
+
     <div class="scroll">
-      {data.map(function (item, index) {
-        return (
-          <div key={index}>
-            <p>{item.name}</p>
-            <p>{item.formatted_address}</p>
-          </div>
-        );
-      })}
+      <Box sx={{ width: "100%" }}>
+        <Stack>
+          {data.map(function (item, index) {
+            return (
+              <div key={index}>
+                <Item>
+                  <h3>{item.name}</h3>
+                  <p>{item.formatted_address}</p>
+                </Item>
+              </div>
+            );
+          })}
+        </Stack>
+      </Box>
     </div>
   );
 }
